abc7cb369c1266253013aa2697e075d07d57a3f7
Jonathan Silva <jonathan.silva167@gmail.com>
2023-12-24 23:55:23 -0500

    Add SQLite database functionality

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SQLite from 'expo-sqlite';
 
+// Open the database
 
 
 export default function App() {
+  const db = SQLite.openDatabase('workoutTracker.db');
 
+  db.transaction(tx => {
+    tx.executeSql("SELECT name FROM sqlite_master WHERE type='table'", [], (_, { rows }) => {
+      const userTables = rows._array
+        .map(row => row.name)
+        .filter(name => name !== 'sqlite_sequence'); // Exclude system tables
+      console.log('User tables:', userTables);
+    });
+  });
 
   return (
     <View style={styles.container}>
